70f91515aa25181d54169ebb6f692c1553c2a9b8
donggeol <dg1106@naver.com>
2021-06-06 22:16:08 +0900

    Update particle filter.cpp

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -21,6 +21,11 @@
 using std::string;
 using std::vector;
 
+#define EPS (0.00001)
+
+double multiv_prob(double sig_x, double sig_y, double x_obs, double y_obs,
+                   double mu_x, double mu_y);
+
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
   /**
    * TODO: Set the number of particles. Initialize all particles to 
@@ -30,8 +35,39 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 0;  // TODO: Set the number of particles
+  std::default_random_engine gen;
+  
+  if (is_initialized) {
+    return;
+  }
+  
+  num_particles = 30;  // TODO: Set the number of particles
 
+  Particle new_particle;
+  double std_x = std[0];
+  double std_y = std[1];
+  double std_theta = std[2];
+  
+  // Creating normal distributions
+  std::normal_distribution<double> dist_x(x, std_x);
+  std::normal_distribution<double> dist_y(y, std_y);
+  std::normal_distribution<double> dist_theta(theta, std_theta);
+  
+  // Generate particles with normal distribution with mean on GPS values.
+  for (int i = 0; i < num_particles; ++i){
+    new_particle.id = i;
+    new_particle.x = dist_x(gen);
+    new_particle.y = dist_y(gen);
+    new_particle.theta = dist_theta(gen);
+    new_particle.weight = 1.0;
+    
+    particles.push_back(new_particle);
+  }
+  
+  // This filter is now initialized
+  is_initialized = true;
+  
+  std::cout << "Particle init done" << std::endl;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
@@ -43,7 +79,40 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
    *  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
    *  http://www.cplusplus.com/reference/random/default_random_engine/
    */
-
+  std::default_random_engine gen;
+  
+  // Extracting standard deviations
+  double std_x = std_pos[0];
+  double std_y = std_pos[1];
+  double std_theta = std_pos[2];
+  
+  // Creating normal distributions
+  std::normal_distribution<double> dist_x(0, std_x);
+  std::normal_distribution<double> dist_y(0, std_y);
+  std::normal_distribution<double> dist_theta(0, std_theta);
+  
+  // Calculate new state
+  for (int i = 0; i < num_particles; i++){
+   
+    double theta = particles[i].theta;
+    
+    if (fabs(yaw_rate < EPS)) {
+      particles[i].x += velocity * delta_t * cos(theta);
+      particles[i].y += velocity * delta_t * sin(theta); 
+    }
+    else {
+      particles[i].x += velocity / yaw_rate * (sin(theta + yaw_rate * delta_t) - sin(theta));
+      particles[i].y += velocity / yaw_rate * (cos(theta) - cos(theta + yaw_rate * delta_t));
+      particles[i].theta += yaw_rate * delta_t;
+    }
+    
+    // Adding noise
+    particles[i].x += dist_x(gen);
+    particles[i].y += dist_y(gen);
+    particles[i].theta += dist_theta(gen);
+  }
+ 
+  std::cout << "Prediction step done" << std::endl;
 }
 
 void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted, 
@@ -56,7 +125,20 @@ void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted,
    *   probably find it useful to implement this method and use it as a helper 
    *   during the updateWeights phase.
    */
-
+  
+  for (auto& obs: observations) {
+    double minD = std::numeric_limits<float>::max();
+    
+    for (const auto& pred: predicted) {
+     double distance = dist(obs.x, obs.y, pred.x, pred.y);
+      if (minD > distance) {
+        minD = distance;
+        obs.id = pred.id;
+      }
+    }
+  }
+  
+  std::cout << "Association step done" << std::endl;
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
@@ -75,6 +157,66 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
    *   and the following is a good resource for the actual equation to implement
    *   (look at equation 3.33) http://planning.cs.uiuc.edu/node99.html
    */
+  
+  for (auto& p: particles){
+    p.weight = 1.0;
+    
+    // step 1. Find landmarks in range of particles
+    vector<LandmarkObs> inRangeLandmarks;
+    
+    double particle_x = p.x;
+    double particle_y = p.y;
+    //double particle_theta = p.theta;
+    
+    for (const auto& lm: map_landmarks.landmark_list){
+      double sqrt_snsr_range = dist(particle_x, particle_y, lm.x_f, lm.y_f);
+      if (sqrt_snsr_range < sensor_range) {
+        // if the landmark is within the sensor range, save it to predictions
+        inRangeLandmarks.push_back(LandmarkObs{lm.id_i, lm.x_f, lm.y_f});
+      }
+    }
+    
+    // step 2. Transform observation coordinates from vehicle to map
+    vector<LandmarkObs> mappedObservations;
+    double cos_theta = cos(p.theta);
+    double sin_theta = sin(p.theta);
+    
+    for (const auto& obs: observations){
+      LandmarkObs tmp;
+      tmp.x = obs.x * cos_theta - obs.y * sin_theta + p.x;
+      tmp.y = obs.x * sin_theta - obs.y * cos_theta + p.y;
+      
+      mappedObservations.push_back(tmp);
+    }
+    
+    // step 3. find landmark index for each observation
+    dataAssociation(inRangeLandmarks, mappedObservations);
+    
+    // step 4. compute the particle's weight:
+    for (const auto& obs_m: mappedObservations){
+      int target_m = obs_m.id - 1;
+      Map::single_landmark_s landmark = map_landmarks.landmark_list.at(target_m);
+      
+      // define inputs
+      double sig_x, sig_y, x_obs, y_obs, mu_x, mu_y;
+      double weight;
+      
+      sig_x = std_landmark[0];
+      sig_y = std_landmark[1];
+      x_obs = obs_m.x;
+      y_obs = obs_m.y;
+      mu_x  = landmark.x_f;
+      mu_y  = landmark.y_f;
+      
+      // Calculate Obs weight
+      weight = multiv_prob(sig_x, sig_y, x_obs, y_obs, mu_x, mu_y);
+      std::cout << "Weight: " << weight << std::endl;
+      
+      p.weight *= weight;
+    }
+    
+    weights.push_back(p.weight);
+  }
 
 }
 
@@ -85,7 +227,43 @@ void ParticleFilter::resample() {
    * NOTE: You may find std::discrete_distribution helpful here.
    *   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
    */
-
+  
+  // Get weights and max weight
+  std::default_random_engine gen;
+  
+  vector<double> weights;
+  double maxWeight = std::numeric_limits<double>::min();
+  for (auto& p: particles) {
+    weights.push_back(p.weight);
+    
+    if (p.weight > maxWeight) {
+      maxWeight = p.weight;
+    }
+  }
+  
+  // Creating distributions
+  std::uniform_real_distribution<double> distDouble(0.0, maxWeight);
+  std::uniform_int_distribution<int> distInt(0, num_particles - 1);
+  
+  // Generating index
+  int index = distInt(gen);
+  
+  double beta = 0.0;
+  
+  // the wheel
+  vector<Particle> resampledParticles;
+  for (auto& p: particles) {
+    beta += distDouble(gen) * 2.0;
+    while (beta > weights[index]) {
+      beta -= weights[index];
+      index = (index + 1) % num_particles;
+    }
+    resampledParticles.push_back(particles[index]);
+  }
+  
+  particles = resampledParticles;
+  
+  std::cout << "Resample step done" << std::endl;
 }
 
 void ParticleFilter::SetAssociations(Particle& particle, 
@@ -125,4 +303,22 @@ string ParticleFilter::getSenseCoord(Particle best, string coord) {
   string s = ss.str();
   s = s.substr(0, s.length()-1);  // get rid of the trailing space
   return s;
+}
+
+double multiv_prob(double sig_x, double sig_y, double x_obs, double y_obs,
+                   double mu_x, double mu_y) {
+  // calculate normalization term
+  double gauss_norm;
+  gauss_norm = 1 / (2 * M_PI * sig_x * sig_y);
+
+  // calculate exponent
+  double exponent;
+  exponent = (pow(x_obs - mu_x, 2) / (2 * pow(sig_x, 2)))
+               + (pow(y_obs - mu_y, 2) / (2 * pow(sig_y, 2)));
+    
+  // calculate weight using normalization terms and exponent
+  double weight;
+  weight = gauss_norm * exp(-exponent);
+    
+  return weight;
 }
